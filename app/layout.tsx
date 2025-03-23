566bc3975cd1ae8838244a42c64c6bf9483da18a
jackere1 <jackerenh@gmail.com>
2025-03-23 21:03:49 +0800

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Хуулийн зөвлөгөө",
+  description: "Иргэдэд нээлттэй, хуулийн дата дээр суурилсан, бодит зөвлөгөө үзүүлэх систем",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
